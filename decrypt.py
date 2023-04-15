f1c4a1cbadfbf0d20ba8fd6216530cbcca7337ab
Katrina Pabuna <kinapabuna@gmail.com>
2023-04-15 19:31:59 +0800

    Check every character

--- decrypt.py
+++ decrypt.py
@@ -3,7 +3,10 @@
 # Pseudocode
 # Ask the user for an encrypted text then save it
 encrypted_str = input("Enter a string to decrypt: ")
+
 # Check every character
+for i in range(len(encrypted_str)):
+
 # Convert '*' to 'a'
 # Convert '&' to 'e'
 # Convert '#' to 'i'
