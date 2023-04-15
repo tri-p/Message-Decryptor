c089fccc9c611d3a5856b9af1fa165ad163a39e6
Katrina Pabuna <kinapabuna@gmail.com>
2023-04-15 19:29:45 +0800

    Ask the user for an encrypted text then save it

---
+++ decrypt.py
@@ -0,0 +1,12 @@
+# PABUNA, KATRINA B.
+# PROBLEM 2: DECRYPTION 
+# Pseudocode
+# Ask the user for an encrypted text then save it
+encrypted_str = input("Enter a string to decrypt: ")
+# Check every character
+# Convert '*' to 'a'
+# Convert '&' to 'e'
+# Convert '#' to 'i'
+# Convert '+' to 'o'
+# Convert '!' to 'u'
+# Print the output
